24496ebaf4f02e44583eb44edf186c116cdd80a9
= <=>
2023-04-04 19:24:23 +0600

    Add loading skeleton

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { Grid, Box } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Skeleton from "@mui/material/Skeleton";
+
+export default function GameCardSkeleton() {
+  const length = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3];
+  return (
+    <React.Fragment>
+      <Grid container>
+        {length.map((i, l) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} padding={1} key={i}>
+            <Card sx={{ width: "100%", borderRadius: 2 }}>
+              <Skeleton
+                sx={{ height: 170, width: "100%" }}
+                animation="wave"
+                variant="rectangular"
+              />
+
+              <CardContent>
+                <React.Fragment>
+                  <Box sx={{ display: "flex" }}>
+                    <Skeleton
+                      animation="wave"
+                      height={35}
+                      style={{ marginBottom: 6, flex: 4 }}
+                    />
+                    <Box sx={{ flex: 3 }}></Box>
+                    <Skeleton
+                      animation="wave"
+                      height={35}
+                      style={{ marginBottom: 6, flex: 1 }}
+                    />
+                  </Box>
+                  <Skeleton
+                    animation="wave"
+                    height={35}
+                    style={{ marginBottom: 6, flex: 1 }}
+                  />
+                </React.Fragment>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </React.Fragment>
+  );
+}
--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -1,9 +1,10 @@
 import { Grid, Typography } from "@mui/material";
 import { useGames } from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const Games = () => {
-  const { error, games } = useGames();
+  const { error, games, isLoading } = useGames();
   return (
     <>
       {error && (
@@ -12,11 +13,17 @@ const Games = () => {
         </Typography>
       )}
       <Grid container>
-        {games.map((game) => (
-          <Grid item key={game.id} xs={12} sm={6} md={4} lg={3}>
-            <GameCard game={game} />
-          </Grid>
-        ))}
+        {isLoading ? (
+          <GameCardSkeleton />
+        ) : (
+          <>
+            {games.map((game) => (
+              <Grid item key={game.id} xs={12} sm={6} md={4} lg={3}>
+                <GameCard game={game} />
+              </Grid>
+            ))}
+          </>
+        )}
       </Grid>
     </>
   );
